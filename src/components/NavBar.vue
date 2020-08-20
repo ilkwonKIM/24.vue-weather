b85b74f29f2140048baee9a1aaa0cf6f00a3a4a5
ilkwonKIM <ilkim226@gmail.com>
2020-08-20 12:23:38 +0900

    NavBarVue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+	.header
+		.d-flex.align-items-center.bg-dark.text-light-gray
+			.logo.p-3.mr-2
+				i.fa.fa-sun.text-light
+			router-link(to='/')
+				.p-2.mr-2 Home
+			router-link(to='daily')
+				.p-2.mr-2 Daily Weather
+			router-link(to='weekly')
+				.p-2.mr-2 weekly Weather
+</template>
+
+<script>
+export default {
+	name: 'NavBar'
+}
+</script>
+
+<style scoped lang="less">
+	a {
+		text-decoration: none;
+		&:link {color:#fff1e0; text-decoration: none;}
+		&:visited {color:#fff1e0;text-decoration: none;}
+		&:active {color:#fff1e0; text-decoration: none;}
+		&:hover {color:#ebc492; text-decoration: none;}
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,17 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+//전역으로 사용 할 module은 main.js에서 선언한다.
+import { BootstrapVue,IconsPlugin } from 'bootstrap-vue';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: h => h(App)
 }).$mount('#app')
