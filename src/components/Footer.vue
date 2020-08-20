78fe542094a2c957eb4691ae9b27a38df60ce56f
ilkwonKIM <ilkim226@gmail.com>
2020-08-20 13:18:22 +0900

    Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+	.footer-wrap.bg-dark.text-light
+		.p-3.text-center &copy; ilkwonKim
+</template>
+
+<script>
+export default {
+	name: 'Footer'
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
