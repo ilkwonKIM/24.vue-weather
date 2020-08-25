2e273f4b62a95f4622f8741f714039a02372c886
ilkwonKIM <ilkim226@gmail.com>
2020-08-25 15:05:29 +0900

    weeklydater생성 완료

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import {windDir, iconUrl} from '../modules/util.js'
+import moment from 'moment'
 
 const API_KEY = '0a3cf0327ffa6fed3238a04bf2a636c4' // openwather 사이트에서 로그인하고 API_KEY
 const CITY_URL = '/json/city.json';
@@ -23,11 +24,16 @@ const axWeather = async (cityId) => {
 		const daily = await axios.get(DAILY_URL, { params });
 		const weekly = await axios.get(WEEKLY_URL, { params });
 		//Daily
-		daily.data.icon = iconUrl(daily.data.weather[0].icon, ICON_URL)
-		daily.data.windDir = windDir(daily.data.wind.deg)
+		daily.data.icon = iconUrl(daily.data.weather[0].icon, ICON_URL);
+		daily.data.windDir = windDir(daily.data.wind.deg);
 
 		//Weekly
-		
+		for (let v of weekly.data.list ){
+			v.icon = iconUrl(v.weather[0].icon, ICON_URL);
+			v.date = moment(Number(v.dt) * 1000).format('YYYY년 MM월 DD일 HH시');
+			v.windDir = windDir(v.wind.deg);
+		}
+
 		return { daily: daily.data, weekly: weekly.data };
 	}//axios는 객체로 params여야지 데이터를 받아온다 data로 했을때는 error남
 	catch(e) {
